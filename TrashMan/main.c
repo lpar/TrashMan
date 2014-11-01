f56f4f2cec830c07e6cc9d93de683e54c1e96004
mathew <meta@keynsham.local>
2014-11-01 12:09:33 -0500

    Initial Commit

---
+++ TrashMan/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TrashMan
+//
+//  Created by mathew on 2014-11-01.
+//  Copyright (c) 2014 mathew. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
